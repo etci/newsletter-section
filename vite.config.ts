6987d722af1c46c5c66633928ea69f4a123d0889
oke <kutayetci@gmail.com>
2026-02-22 19:13:40 +0100

    chore: configure import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), 'src'),
+    },
+  },
 });
